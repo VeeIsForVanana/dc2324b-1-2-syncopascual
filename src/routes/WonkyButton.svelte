a4095d5245e6c149217d2d8e2a9fd5ac7d88ddac
Marcus Pascual <marcusjoaquim05@gmail.com>
2024-03-11 20:42:10 +0800

    feat: add WonkyButton component code

--- src/routes/WonkyButton.svelte
+++ src/routes/WonkyButton.svelte
@@ -1,1 +1,25 @@
-<p>Hello world!</p>
+<script>
+	let value = 0;
+
+	function increment() {
+		value += Math.floor(Math.random() * 9);
+	}
+</script>
+
+{#if value % 15 == 0}
+	<button on:click={increment}>
+		FizzBuzz {value}
+	</button>
+{:else if value % 3 == 0}
+	<button on:click={increment}>
+		Fizz {value}
+	</button>
+{:else if value % 5 == 0}
+	<button on:click={increment}>
+		Buzz {value}
+	</button>
+{:else}
+	<button on:click={increment}>
+		{value}
+	</button>
+{/if}
